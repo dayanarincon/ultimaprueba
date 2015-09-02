b912872acae6f3be99799421f5ddb9129425821b
pc17 <victorhugorodriguez266@gmail.com>
2015-09-02 16:07:03 -0500

    he terminado el login

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>mi pagina</title>
+</head>
+<body>
+    <?php
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+    echo "hola dayana";
+
+
+    ?>
+</body>
+
+</html>
---
+++ index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>mi pagina</title>
+</head>
+<body>
+    <?php
+    echo "hola karen";
+    echo "hola karen";
+    echo "hola karen";
+
+    ?>
+</body>
+
+</html>
